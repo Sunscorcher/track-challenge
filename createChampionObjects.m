212ef137da32d5a4155b4937c9d306d4bbfe64c7
Jeremy <Jeremy@Jeremy-PC-2023>
2025-05-24 22:49:19 -0400

    Delete

--- createChampionObjects.m
+++
@@ -1,5 +0,0 @@
-% createChampionObjects()
-    % This function instantiates the classes for each champion
-    % Editing this file is not recommended, the app calls it if
-    % challengeData.mat does not exist.
-    AatroxObj = Aatrox;
